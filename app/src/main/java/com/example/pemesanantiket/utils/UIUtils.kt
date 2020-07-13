8cd19a605bf47bfcfc21da8f2263a180b3d7bc46
derysudrajat <dery.sudrajat17@gmail.com>
2020-07-13 20:18:30 +0700

    add barcode generator

--- app/src/main/java/com/example/pemesanantiket/utils/UIUtils.kt
+++ app/src/main/java/com/example/pemesanantiket/utils/UIUtils.kt
@@ -2,9 +2,16 @@ package com.example.pemesanantiket.utils
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import javax.security.auth.callback.Callback
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.io.ByteArrayOutputStream
+
 
 fun showPopupDialog(
         context : Context?,
@@ -30,4 +37,28 @@ fun showToast(
         message: String?
 ){
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+fun generateBarcode(
+        context: Context?,
+        code : String?,
+        callback: (imgUri : String) -> Unit
+){
+    val multiFormatWriter = MultiFormatWriter();
+    try {
+        val bitMatrix = multiFormatWriter
+                .encode(code, BarcodeFormat.CODE_128, 222,52)
+        val barcodeEncoder = BarcodeEncoder()
+        val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+        callback(getImageUri(context!!, bitmap))
+    }catch (e : Exception){
+        Log.d("TAG", "onCreate: $e")
+    }
+}
+
+fun getImageUri(context: Context, inImage: Bitmap): String {
+    val bytes = ByteArrayOutputStream()
+    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+    val path = MediaStore.Images.Media.insertImage(context.contentResolver, inImage, "Title", null)
+    return Uri.parse(path).toString()
 }
